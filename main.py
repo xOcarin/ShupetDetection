ecc766e1035700edb231096da136002e4b446602
Cameron <cschilling16111@gmail.com>
2022-12-08 04:52:34 -0500

    Takes a picture if tepig is detected. needs a fail safe. might switch detection to use feature points instead. If not, feature points will be used to confirm that the pokemon is the same.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import extract_pokemon
 import overlay_pokemon
 
-
+frame = 'data/tepig.png'
 #method for scanning in Tepig
 def chooseTepig(insert = False):
     cap = cv2.VideoCapture(0)
@@ -18,15 +18,30 @@ def chooseTepig(insert = False):
         #cv2.normalize(img, img, 50, 255, cv2.NORM_MINMAX)
         # cv2.rectangle(img, (480, 170), (960, 80), color=(0, 225, 0), thickness=2)
         prediction = maskClassifier.getPrediction(img)
-        # print(prediction)
+
+        pred = str(prediction)
+        listofchars = list(pred)
+        properPred = listofchars[len(listofchars) - 2]
+        print('properPred: ', properPred)
+
 
         if insert:
             posX = 200
             posY = 200
             img = overlay_pokemon.overlay_pokemon(img, posX, posY, prediction[1])   # prediction[1] is argmax value
 
+        #if tepig is spotted, the camera will take a picture.
+        if properPred == '1':
+            print('FUCKSDASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD')
+            ret, frame = cap.read()
+            cv2.imshow('img1', frame)
+            cv2.waitKey(0)& 0xFF == ord('q')
+            #cv2.destroyAllWindows()
+            break
         # display cam, press q to exit
         cv2.imshow("Image", img)
+
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -68,3 +83,4 @@ if __name__ == "__main__":
     # extract_pokemon.test_extract()
     # extract_pokemon.test_segment()
     chooseTepig(insert = True)
+
